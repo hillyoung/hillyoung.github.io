cc32bb5cc57ea7c1c83b26025bdd18fe84501dae
hillyoung <>
2022-09-01 15:32:12 +0800

    添加版权信息、网站备案信息

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,37 +3,40 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
+(function($, undefined) {
     "use strict";
 
     var $document = $(document);
 
-    $document.ready(function () {
+    $document.ready(function() {
 
         var $postContent = $(".post-content");
         $postContent.fitVids();
 
         $(".scroll-down, .title-scroll").arctic_scroll();
 
-        $(".menu-button[href='#'], .nav-cover, .nav-close").on("click", function(e){
+        $(".menu-button[href='#'], .nav-cover, .nav-close").on("click", function(e) {
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
 
+        var year = new Date().getFullYear();
+        var copyrightYear = year == 2022 ? year : "2022-" + year;
+        $("#copyright").text("© " + copyrightYear + " 溧水丘壑网络技术服务 版权所有");
     });
 
     // Arctic Scroll by Paul Adam Davis
     // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $.fn.arctic_scroll = function(options) {
 
         var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+                elem: $(this),
+                speed: 500
+            },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
-        allOptions.elem.click(function (event) {
+        allOptions.elem.click(function(event) {
             event.preventDefault();
             var $this = $(this),
                 $htmlBody = $('html, body'),
@@ -43,12 +46,12 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: toMove }, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
             }
         });
 
